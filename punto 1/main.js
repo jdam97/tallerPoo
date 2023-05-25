eef8f40f4272c5164330d0e1ffea01d6df790ddb
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-05-24 22:56:12 -0500

    feat: :sparkles: punto 1
    agregué el punto 1

--- punto 1/main.js
+++ punto 1/main.js
@@ -1,15 +1,24 @@
-class Persona{
-    constructor(nombre,edad,sexo){
-        this.nombre = nombre
-        this.edad = edad
-        this.sexo = sexo
+let nombre = document.querySelector('#nombre')
+let edad = document.querySelector('#edad')
+let sexo = document.querySelector('#sexo')
+let saludar = document.querySelector('.saludar')
+let form = document.querySelector('#form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    class Persona {
+        constructor(name, edad, sexo) {
+            this.name = name
+            this.edad = edad
+            this.sexo = sexo
+        }
+        saludar() {
+            return `<h2>Hola soy ${this.name}</h2>`
+        }
     }
-    saludar() {
-        return `<h2>Hola soy ${this.nombre}</h2>`
-    }
-}
 
-
-let persona1 = new Persona('Jeanfrak0',20,'machorro')
-
-console.log(persona1.saludar())
+    let persona1 = new Persona(nombre.value, edad.value, sexo.value)
+    saludar.innerHTML = persona1.saludar()
+    form.reset()
+})
